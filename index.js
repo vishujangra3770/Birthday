6a2db0abe76af93ee5161b444480507c90853141
Vishu Jangra <95424066+vishujangra3770@users.noreply.github.com>
2025-03-03 00:07:25 +0530

    index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var S = {
       if (i !== -1) {
         S.UI.simulate(decodeURI(action).substring(i + 3));
       } else {
-        S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|DEAR|BESTIE|JYOTI ❤|HBD JYOTI ❤|');
+        S.UI.simulate('|#countdown 3||HAPPY|BIRTHDAY|DEAR|BESTIE|JYOTI ❤|HBD JYOTI ❤');
       }
   
       S.Drawing.loop(function () {
